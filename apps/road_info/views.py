00b7cd8f2af81dfb5eaa5a5aa3fc826d4dd9fcda
cmx_company <569578851@qq.com>
2019-12-24 14:37:24 +0800

    构造数据写excel

--- apps/road_info/views.py
+++ apps/road_info/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.views import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import *
+from myutils.utils import export_excel
+from datetime import datetime
+from devices_mgmt.settings import MEDIA_ROOT
 
 
 class Test(View):
@@ -73,3 +76,115 @@ class Test(View):
         return JsonResponse({
             "status": "success"
         })
+
+
+class ExportView(View):
+    def get(self, request):
+        road_device = RoadDeviceModel.objects.all()
+        data_list = list()
+        for i in road_device:
+            camera_list = list()
+            terminal_list = list()
+            other_list = list()
+            owner_company = i.owner_company
+            if owner_company:
+                owner_company = owner_company.owner_company
+            else:
+                owner_company = ""
+            build_company = i.build_company
+            if build_company:
+                build_company = build_company.build_company
+            else:
+                build_company = ""
+            brigade = i.brigade
+            if brigade:
+                brigade = brigade.brigade
+            else:
+                brigade = ""
+            time = i.time
+            if time:
+                time = datetime.strftime(time, "%Y{}%m{}%d{}").format("年", "月", "日")
+            else:
+                time = ""
+
+            longitude = i.longitude
+            latitude = i.latitude
+            if longitude and latitude:
+                longitude_latitude = longitude + "," + latitude
+            else:
+                longitude_latitude = ""
+            src = MEDIA_ROOT + '/%s' % i.map_img
+            src2 = MEDIA_ROOT + '/%s' % i.img
+            camera_device = CameraDeviceModel.objects.filter(road_device_id=i.id)
+            # print(camera_device)
+            for c in camera_device:
+                terminal = c.terminal
+                if terminal:
+                    terminal = terminal.ip
+                else:
+                    terminal = ""
+                server = c.server
+                if server:
+                    server = server.ip
+                else:
+                    server = ""
+                wf_type = c.wf_type
+                if wf_type:
+                    wf_type = ",".join(wf_type)
+                else:
+                    wf_type = ""
+                camera_list.append({
+                    "ip": c.ip,
+                    "device_type": c.device_type,
+                    "username_pwd": c.username + "/" + c.password,
+                    "terminal": terminal,
+                    "server": server,
+                    "wf_type": wf_type
+                })
+            terminal_device = TerminalDeviceModel.objects.filter(road_device_id=i.id)
+            for t in terminal_device:
+                server = t.server
+                if server:
+                    server = server.ip
+                else:
+                    server = ""
+                terminal_list.append({
+                    "ip": t.ip,
+                    "device_type": t.device_type,
+                    "username_pwd": t.username + "/" + t.password,
+                    "terminal": "",
+                    "server": server,
+                    "wf_type": ""
+                })
+            other_device = OtherDeviceModel.objects.filter(road_device_id=i.id)
+            for o in other_device:
+                other_list.append({
+                    "device_name": o.device_name,
+                    "device_type": o.device_type,
+                    "device_num": o.device_num
+                })
+
+            data_list.append({
+                "road_type": i.get_road_type_display(),
+                "road_code": i.road_code,
+                "address": i.address,
+                "owner_company": owner_company,
+                "longitude_latitude": longitude_latitude,
+                "build_company": build_company,
+                "device_code": i.device_code,
+                "brigade": brigade,
+                "time": time,
+                "camera_list": camera_list,
+                "terminal_list": terminal_list,
+                "other_list": other_list,
+                "map_img": src,
+                "img2": src2
+
+            })
+
+        save_file = datetime.strftime(datetime.now(), "%Y%m%d_%H%M%S") + ".xlsx"
+        folder = "/".join((MEDIA_ROOT + "\\" + save_file).split("\\"))
+        # url = "192.168.31.54:9999/media/" + save_file
+        url = "111.230.246.188:8888/media/" + save_file
+        export_excel(data_list, folder)
+        return HttpResponse(url)
--- devices_mgmt/urls.py
+++ devices_mgmt/urls.py
@@ -17,11 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.static import serve
 from .settings import MEDIA_ROOT
-from road_info.views import Test
+from road_info.views import Test, ExportView
 
 
 urlpatterns = [
     url(r'^', admin.site.urls),
     url(r'^test/$', Test.as_view()),
+    url(r'^export/$', ExportView.as_view()),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
