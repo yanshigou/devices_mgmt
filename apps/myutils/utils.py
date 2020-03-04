05409565f9cd758a7c64488eaf0bc233f014a28c
cmx_company <569578851@qq.com>
2020-03-04 17:07:17 +0800

    百度转高德

--- apps/myutils/utils.py
+++ apps/myutils/utils.py
@@ -7,6 +7,7 @@ from openpyxl.styles import colors
 from openpyxl.styles import Alignment
 from openpyxl.drawing.image import Image
 from openpyxl.styles import Border, Side
+import math
 
 data_dict = [
     {
@@ -591,6 +592,31 @@ def export_excel(data_dict, save_file):
 
     # wb.save("test.xlsx")
     wb.save(save_file)
+
+
+# 坐标转换
+
+x_pi = 3.14159265358979324 * 3000.0 / 180.0
+pi = 3.1415926535897932384626  # π
+a = 6378245.0  # 长半轴
+ee = 0.00669342162296594323  # 偏心率平方
+
+
+def bd09_to_gcj02(bd_lon, bd_lat):
+    """
+    百度坐标系(BD-09)转火星坐标系(GCJ-02)
+    百度——>谷歌、高德
+    :param bd_lat:百度坐标纬度
+    :param bd_lon:百度坐标经度
+    :return:转换后的坐标列表形式
+    """
+    x = bd_lon - 0.0065
+    y = bd_lat - 0.006
+    z = math.sqrt(x * x + y * y) - 0.00002 * math.sin(y * x_pi)
+    theta = math.atan2(y, x) - 0.000003 * math.cos(x * x_pi)
+    gg_lng = z * math.cos(theta)
+    gg_lat = z * math.sin(theta)
+    return [gg_lng, gg_lat]
 
 
 if __name__ == '__main__':
