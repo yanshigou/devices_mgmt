c42a30c9278cd104fe444272e717ea30bd9e829d
cmx_company <569578851@qq.com>
2020-03-04 17:08:30 +0800

    map

--- devices_mgmt/urls.py
+++ devices_mgmt/urls.py
@@ -17,13 +17,14 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.static import serve
 from .settings import MEDIA_ROOT
-from road_info.views import Test, ExportView, ModifyRoadCodeView
+from road_info.views import Test, ExportView, ModifyRoadCodeView, ShowMapView
 
 
 urlpatterns = [
     url(r'^', admin.site.urls),
     url(r'^test/$', Test.as_view()),
     url(r'^export/$', ExportView.as_view()),
+    url(r'^map/$', ShowMapView.as_view(), name='show_map'),
     # url(r'^modifyWT/$', ModifyRoadCodeView.as_view()),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
